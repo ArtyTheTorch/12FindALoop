353ea7d534b28161063c339c181cb3696f0409f8
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:53:23 -0700

    TODO: Finds a loop in the LinkedList
    Found the Tortoise and Hare algorithm that needs to be implemented.
    However, Need to figure out how to create a "loop". Im sure its simple
    just haven't figured it out yet...
    TODO: Create a test case
    UI-Implementation

---
+++ 12FindALoop/FindLoop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _12FindALoop
+{
+    class FindLoop
+    {
+        LinkedList<object> list = new LinkedList<object>();
+        public FindLoop() {
+            int[] sampleData = {1,2,3,4,5,6,7,8,9,10};
+            foreach (int i in sampleData) {
+                list.AddFirst(i);
+            }
+            //list.Last = list.First;
+            // I need to create a loop in the linked list somewhere here...
+        }
+        public string hasLoop()
+        {
+            // This should be the correct algorithm that detects the loop
+            // It is commonly refered to as the Tortoise and Hare Algorithm
+            LinkedListNode<object> slowNode = list.First;
+            LinkedListNode<object> fastNode1 = list.First;
+            LinkedListNode<object> fastNode2 = list.First;
+
+            while (slowNode != null && fastNode1 == fastNode2.Next && fastNode2 == fastNode1.Next)
+            {
+                if (slowNode == fastNode1 || slowNode == fastNode2)
+                {
+                    return "True";
+                }
+                slowNode = slowNode.Next;
+            }
+            return "False";
+        }
+    }
+}
---
+++ 12FindALoop/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _12FindALoop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FindLoop x = new FindLoop();
+            label2.Text = x.hasLoop();
+        }
+    }
+}
